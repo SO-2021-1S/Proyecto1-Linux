e560488cc51320be392a082f32af6760b4b1e991
fulqjulio <juliancarvajal93f@gmail.com>
2021-07-26 17:55:25 -0500

    Permits

--- proyect/tags/templatetags/define_select.py
+++ proyect/tags/templatetags/define_select.py
@@ -1,6 +1,8 @@
+import os
 from pathlib import Path
 from typing import Any
 from django import template
+from subprocess import getoutput
 
 register = template.Library()
 
@@ -9,5 +11,18 @@ def name(request):
   return request.GET.get('Name', '')
 
 @register.simple_tag
-def tipe(request):
+def type(request):
   return request.GET.get('Tipo', '')
+
+@register.simple_tag
+def permits(request):
+  return request.GET.get('Permisos', '')
+
+@register.simple_tag
+def showpermits(request, rutashow):
+  if request.GET.get('Name', '')=='':
+    comando = "ls -l %s" % os.path.join(rutashow, request.GET.get('Nombre', ''))
+    out = getoutput(comando, shell=True)
+    
+
+  return request.GET.get('Permisos', '')
